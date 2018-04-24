05e4e3335019e5de0a3d1f131d54bbfb3bce8c35
kbaran <kbaran1@bitbucket1.org/kbaran1/cmps128_kbaran.git>
2018-04-23 20:03:12 -0700

    Beginning with network functionality. API most likely being used will be Nearby Connections.

--- app/src/main/java/com/example/a121game/MainActivity.java
+++ app/src/main/java/com/example/a121game/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.a121game;
 
+import android.Manifest;
+import android.content.IntentFilter;
+
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //if the user has not yet allowed the device to access location data, will prompt for permission
+        //am currently unsure how we should handle declined permissions (probably exit app), as the connectivity api that will most likely be used
+        //will require location data.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
+
+
+        }
     }
 }
