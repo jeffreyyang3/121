956f3cbb0454a5bcc8447c03a4a5696618f004fc
kindon <kindonsmith@gmail.com>
2018-04-30 14:57:02 -0700

    Updated player.java class to include a private health variable and public name/character names.

--- app/src/main/java/com/example/a121game/player.java
+++ app/src/main/java/com/example/a121game/player.java
@@ -2,5 +2,19 @@ package com.example.a121game;
 public class player {
     public String PlayerName;
     public String CharacterName;
-    
+
+    private int Health;
+
+    public int GetHealth(){
+        return Health;
+    }
+    public void modifyHealth(int change){
+        this.Health += change;
+    }
+
+    public player(String PlayName, String CharName, int HP){
+        this.PlayerName = PlayName;
+        this.CharacterName = CharName;
+        this.Health = HP;
+    }
 }
