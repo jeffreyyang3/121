f4a3e0bad8aa951b6a29821181d2b0be3ad194d9
kbaran <kbaran1@bitbucket1.org/kbaran1/cmps128_kbaran.git>
2018-05-21 22:44:35 -0700

    Fixed XML issues, added PlayerCreateActivity that is accessible by pressing the create button in the menu bar of CharacterSelectActivity.

--- app/src/main/java/com/example/a121game/CharacterSelectActivity.java
+++ app/src/main/java/com/example/a121game/CharacterSelectActivity.java
@@ -1,9 +1,11 @@
 package com.example.a121game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CharacterSelectActivity extends AppCompatActivity{
 
@@ -21,5 +23,15 @@ public class CharacterSelectActivity extends AppCompatActivity{
         MenuInflater inf = getMenuInflater();
         inf.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Add_Character:
+                startActivity(new Intent(CharacterSelectActivity.this, PlayerCreateActivity.class));
+            default:
+                super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/a121game/PlayerCreateActivity.java
@@ -0,0 +1,13 @@
+package com.example.a121game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_create2);
+    }
+}
